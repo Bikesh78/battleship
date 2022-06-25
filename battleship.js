04a26596ae6ce8ff8f20cc3a6c467ab5be23ee4b
Bikesh Rai <bikeshrai78@gmail.com>
2022-06-25 20:03:06 +0545

    Change test for placing ship

--- battleship.js
+++ battleship.js
@@ -1,3 +1,4 @@
+let shipList = [];
 function shipFactory(name) {
   let length;
   switch (name) {
@@ -19,10 +20,13 @@ function shipFactory(name) {
     default:
       return;
   }
+  const id = name;
   const hitPositions = [];
-  const hit = ([x, y]) => {
-    hitPositions.push([x, y]);
-    return hitPositions[hitPositions.length - 1];
+  const hit = (position) => {
+    /*  hitPositions.push([x, y]);
+    return hitPositions[hitPositions.length - 1]; */
+    hitPositions.push(position);
+    return hitPositions;
   };
   /*   const shipInitialCoordinates = [1, 1];
     const shipDirection = "horizontal";
@@ -63,63 +67,89 @@ function shipFactory(name) {
       return shipCoordinates;
     }
   };
-  return { length, hit, isSunk };
+  shipList.push({ id, length, hit, isSunk });
+  return { id, length, hit, isSunk };
 }
 
 function gameBoardFactory() {
   // create gameBoard Array
   const gameBoard = [];
-  for (let i = 0; i < 10; i++) {
-    for (let j = 0; j < 10; j++) {
-      gameBoard.push([i, j]);
-    }
+  for (let i = 0; i < 100; i++) {
+    gameBoard.push("");
   }
 
   let shipDirection = "";
   const setShipDirection = (direction) => {
     return (shipDirection = direction);
   };
-
-  const shipCoordinates = [];
-  const placeShip = (shipName, initialPosition) => {
-    const ship = shipFactory(shipName);
-    const shipLength = ship.length;
-    if (
-      shipLength + initialPosition[0] > 10 ||
-      shipLength + initialPosition[1] > 10
-    ) {
+  const getShipPosition = (shipId) => {
+    const shipPosition = [];
+    gameBoard.forEach((item, index) => {
+      if (item === shipId) {
+        // return index;
+        shipPosition.push(index);
+      }
+    });
+    return shipPosition;
+  };
+  // const shipCoordinates = [];
+  const placeShip = (shipId, initialPosition) => {
+    // const ship = shipFactory(shipName);
+    const ship = shipList.filter((item) => item.id == shipId);
+    const shipLength = ship[0].length;
+    /* if (shipLength + initialPosition > 10) {
       return "Not valid";
+    } */
+
+    function isMoveValid(shipDirection) {
+      if (shipDirection === "horizontal") {
+        // All the rows have same tens values e.g first row: 0 to 9, second row: 10 to 19.
+        // Having different tens values means the ship has exceeded its horizontal limit and move is invalied
+        const lastShipCoordinate = initialPosition + shipLength;
+        const initialTensValue = Math.floor(initialPosition / 10);
+        const lastTensValue = Math.floor(lastShipCoordinate / 10);
+        if (initialTensValue !== lastTensValue) {
+          return false;
+        }
+        return true;
+      } else if (shipDirection === "vertical") {
+        // The coordiantes in gameboard ranges from 0 to 99.
+        // So the ship's coordinate should not be greater than 99
+        const lastShipCoordinate = initialPosition + (shipLength - 1) * 10;
+        if (lastShipCoordinate > 99) {
+          return false;
+        }
+        return true;
+      }
     }
     if (shipDirection === "horizontal") {
-      let i = 0;
-      while (i < shipLength) {
-        shipCoordinates.push(
-          initialPosition.map((item, index) => {
-            if (index === 0) {
-              return item + i;
-            } else {
-              return item;
-            }
-          })
-        );
-        i++;
+      if (isMoveValid(shipDirection)) {
+        let i = 0;
+        while (i < shipLength) {
+          gameBoard[initialPosition + i] = ship[0].id;
+          i++;
+        }
+      } else {
+        return "Not Valid";
       }
     } else if (shipDirection === "vertical") {
-      let i = 0;
-      while (i < shipLength) {
-        shipCoordinates.push(
-          initialPosition.map((item, index) => {
-            if (index === 1) {
-              return item + i;
-            } else {
-              return item;
-            }
-          })
-        );
-        i++;
+      if (isMoveValid(shipDirection)) {
+        let i = 0;
+        while (i < shipLength) {
+          if (i === 0) {
+            gameBoard[initialPosition + i] = ship[0].id;
+          } else {
+            gameBoard[initialPosition + 10 * i] = ship[0].id;
+          }
+          i++;
+        }
+      } else {
+        return "Not Valid";
       }
     }
-    return shipCoordinates;
+    // return shipCoordinates;
+    // return getShipPosition(shipId);
+    return gameBoard;
   };
 
   const receiveAttack = (coordinates) => {
@@ -131,7 +161,7 @@ function gameBoardFactory() {
     });
     return solution;
   };
-  return { setShipDirection, placeShip, receiveAttack };
+  return { setShipDirection, placeShip, getShipPosition, receiveAttack };
 }
 
 export { shipFactory, gameBoardFactory };
--- battleship.test.js
+++ battleship.test.js
@@ -7,53 +7,49 @@ test("get ship length", () => {
 });
 test("get hit location", () => {
   const ship = shipFactory("submarine");
-
-  expect(ship.hit([1, 1])).toStrictEqual([1, 1]);
+  expect(ship.hit(15)).toStrictEqual([15]);
 });
 test("check horizontally placed ship is sunk", () => {
   const ship = shipFactory("submarine");
-  ship.hit([1, 1]);
-  ship.hit([1, 2]);
-  ship.hit([1, 3]);
-  const shipCoordinates = [
-    [1, 1],
-    [1, 2],
-    [1, 3],
-  ];
+  ship.hit(1);
+  ship.hit(2);
+  ship.hit(3);
+  const shipCoordinates = [1, 2, 3];
   expect(ship.isSunk(shipCoordinates)).toBe("Sunk");
 });
 
 test("place submarine horizontally in gameboard", () => {
   const gameBoard = gameBoardFactory();
-  //   const ship = shipFactory("submarine");
+  const ship = shipFactory("cruiser");
   gameBoard.setShipDirection("horizontal");
-  const initialPosition = [1, 1];
-  expect(gameBoard.placeShip("submarine", initialPosition)).toStrictEqual([
-    [1, 1],
-    [2, 1],
-    [3, 1],
-  ]);
+  const initialPosition = 1;
+  gameBoard.placeShip("cruiser", initialPosition);
+  expect(gameBoard.getShipPosition("cruiser")).toStrictEqual([1, 2, 3]);
 });
 
 test("place carrier vertically in gameboard", () => {
   const gameBoard = gameBoardFactory();
+  const ship = shipFactory("carrier");
   gameBoard.setShipDirection("vertical");
-  const initialPosition = [2, 2];
-  expect(gameBoard.placeShip("carrier", initialPosition)).toStrictEqual([
-    [2, 2],
-    [2, 3],
-    [2, 4],
-    [2, 5],
-    [2, 6],
+  const initialPosition = 11;
+  gameBoard.placeShip("carrier", initialPosition);
+  expect(gameBoard.getShipPosition("carrier")).toStrictEqual([
+    11, 21, 31, 41, 51,
   ]);
 });
 test("place ship out of boundary", () => {
   const gameBoard = gameBoardFactory();
   gameBoard.setShipDirection("horizontal");
-  const initialPosition = [1, 7];
-  expect(gameBoard.placeShip("carrier", initialPosition)).toBe("Not valid");
+  const initialPosition = 7;
+  expect(gameBoard.placeShip("carrier", initialPosition)).toBe("Not Valid");
 });
-test("receive attack", () => {
+test("place ship out of boundary", () => {
+  const gameBoard = gameBoardFactory();
+  gameBoard.setShipDirection("vertical");
+  const initialPosition = 80;
+  expect(gameBoard.placeShip("carrier", initialPosition)).toBe("Not Valid");
+});
+test.skip("receive attack", () => {
   const gameBoard = gameBoardFactory();
   gameBoard.setShipDirection("vertical");
   const initialPosition = [2, 2];
