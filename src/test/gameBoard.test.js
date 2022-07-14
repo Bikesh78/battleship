e8fcc2e576d6b9d1f6366337ca5d16acdcf40343
Bikesh Rai <bikeshrai78@gmail.com>
2022-07-14 13:02:13 +0545

    Restore accidentally deleted tests

--- src/test/gameBoard.test.js
+++ src/test/gameBoard.test.js
@@ -3,6 +3,57 @@ import gameBoardFactory from "../gameBoardFactory";
 import shipFactory from "../shipFactory";
 
 describe("game board", () => {
+  test("place cruiser horizontally in gameboard", () => {
+    const gameBoard = gameBoardFactory();
+    const ship = shipFactory("cruiser");
+    gameBoard.setShipDirection(ship, "horizontal");
+    const initialPosition = 1;
+    gameBoard.placeShip(ship, initialPosition);
+    expect(gameBoard.getShipPosition(ship.id)).toStrictEqual([1, 2, 3]);
+  });
+
+  test("place carrier vertically in gameboard", () => {
+    const gameBoard = gameBoardFactory();
+    const ship = shipFactory("carrier");
+    gameBoard.setShipDirection(ship, "vertical");
+    const initialPosition = 11;
+    gameBoard.placeShip(ship, initialPosition);
+    expect(gameBoard.getShipPosition(ship.id)).toStrictEqual([
+      11, 21, 31, 41, 51,
+    ]);
+  });
+  test("place ship out of boundary", () => {
+    const gameBoard = gameBoardFactory();
+    const carrier = shipFactory("carrier");
+    gameBoard.setShipDirection(carrier, "horizontal");
+    const initialPosition = 7;
+    expect(gameBoard.placeShip(carrier, initialPosition)).toBe("Not Valid");
+  });
+  test("place ship out of boundary", () => {
+    const gameBoard = gameBoardFactory();
+    const ship = shipFactory("carrier");
+    gameBoard.setShipDirection(ship, "vertical");
+    const initialPosition = 80;
+    expect(gameBoard.placeShip(ship, initialPosition)).toBe("Not Valid");
+  });
+  test("receive attack", () => {
+    const gameBoard = gameBoardFactory();
+    const ship = shipFactory("carrier");
+    gameBoard.shipList.push(ship);
+    gameBoard.setShipDirection(ship, "vertical");
+    const initialPosition = 50;
+    gameBoard.placeShip(ship, initialPosition);
+    expect(gameBoard.receiveAttack(60)).toBe(60);
+  });
+  test("Receive missed attack", () => {
+    const gameBoard = gameBoardFactory();
+    const ship = shipFactory("carrier");
+    gameBoard.shipList.push(ship);
+    gameBoard.setShipDirection(ship, "vertical");
+    const initialPosition = 50;
+    gameBoard.placeShip(ship, initialPosition);
+    expect(gameBoard.receiveAttack(51)).toBe("Missed");
+  });
   test("Every ship has sunk", () => {
     const gameBoard = gameBoardFactory();
     const submarine = shipFactory("submarine");
